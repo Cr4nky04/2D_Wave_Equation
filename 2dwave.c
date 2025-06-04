64a990fe4cb1d0b6612a456cbba104e272e23f3b
Cr4nky04 <Anh.NV225434@sis.hust.edu.vn>
2025-06-04 15:06:39 +0700

    Update c code

--- 2dwave.c
+++ 2dwave.c
@@ -5,7 +5,7 @@
 #define NX 100
 #define NY 100
 #define STEPS 1000
-#define SAVE_INTERVAL 20   // Save every 100 steps
+#define SAVE_INTERVAL 10   // Save every 100 steps
 
 const double c = 1.0;
 const double dx = 0.01;
